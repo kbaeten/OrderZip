02ea78bbb174d5ff962b76727f1d4da25fe69520
KC Baeten <kcbaeten@KCs-MacBook-Pro.local>
2016-08-29 14:32:11 -0600

    createding db tables

--- migrations/20160829124947_migrations.js
+++ migrations/20160829124947_migrations.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('beers', function(table) {
+    table.increments('id').primary();
+    table.string('brew').notNullable().unique();
+    table.string('package').notNullable();
+    table.string('brand').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('beers');
 };
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
