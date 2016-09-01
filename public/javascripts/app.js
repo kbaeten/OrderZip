4d5558cdea406bc3c359d62ea9445ad96f59aa75
KC Baeten <kcbaeten@KCs-MacBook-Pro.local>
2016-09-01 13:59:36 -0600

    added header

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,8 @@
 var app = angular.module("orderZip", ['angularMoment', 'ngAnimate', 'ngRoute'])
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/landing.html'
+    })
+  }]);
